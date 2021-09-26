e0f1b735fb8a052a49f882feee8ac8fd55b38516
bassil-97 <61696257+bassil-97@users.noreply.github.com>
2021-09-27 01:34:46 +0300

    edits

--- src/components/form-parts/bank-details/BankDetails.js
+++ src/components/form-parts/bank-details/BankDetails.js
@@ -21,6 +21,7 @@ export default function BankDetails({ handleChange, refundAmount }) {
                 });
             }
 
+            console.log(loadedBanksList);
             setBanksList(loadedBanksList);
         };
 
