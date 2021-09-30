639f47bf6a96697b52b41f9341b36e03e5758fc4
bassil-97 <61696257+bassil-97@users.noreply.github.com>
2021-09-30 15:07:48 +0300

    edits

--- src/admin/dashboard-sections/Home.js
+++ src/admin/dashboard-sections/Home.js
@@ -1,9 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Clock from '../../UI/Clock';
 
 import './Dashboard-sections.css';
 
+import axios from 'axios';
+
 export default function Home(props) {
+
+    const [requests, setRequests] = useState();
+
+    useEffect(()=> {
+        fetchRequestsList()
+    }, []);
+
+    const fetchRequestsList = async () => {
+
+        const loadedRequestsList = [];
+
+        axios.get("https://investment-com.herokuapp.com/clients-requests", {
+            headers: {
+                'Access-Control-Allow-Origin': 'https://investment.netlify.app',
+                'Accept': '*',
+            }
+        })
+        .then((response) => {
+            if(response.data['requests']) {
+                for(const key in response.data['requests']) {
+                    loadedRequestsList.push({
+                        name: response.data['requests'][key].user_fullname,
+                        idNumber: response.data['requests'][key].user_id_number,
+                        phoneNumber: response.data['requests'][key].user_phone_number,
+                        refundAmount: response.data['requests'][key].refund_amount,
+                        bankAccount: response.data['requests'][key].user_bank,
+                        cardNumber: response.data['requests'][key].user_card_number,
+                    });
+                }
+                console.log(response.data['requests']);
+                setRequests(loadedRequestsList);
+            }
+        }).catch((error) => {
+            console.log(error);
+        });
+    };
+
     return (
         <div>
             <div className="db-header">
@@ -19,8 +58,8 @@ export default function Home(props) {
                     <div className="data-box" data-aos="zoom-in">
                         <h6>Total requests</h6>
                         {
-                            props.requests 
-                            ? <h4 className="mb-0">{props.requests.length}</h4>
+                            requests 
+                            ? <h4 className="mb-0">{requests.length}</h4>
                             : <h4>0</h4>
                         }
                     </div>
--- src/admin/dashboard/Dashboard.js
+++ src/admin/dashboard/Dashboard.js
@@ -11,54 +11,12 @@ import Requests from '../dashboard-sections/Orders';
 import BankListFrom from '../dashboard-sections/BanksListForm';
 
 
-import axios from 'axios';
-
-
 function Dashboard() {
-
-    const [requests, setRequests] = useState();
-
-    useEffect(()=> {
-        fetchRequestsList()
-    }, []);
-
-    const fetchRequestsList = async () => {
-
-        const loadedRequestsList = [];
-
-        axios.get("https://investment-com.herokuapp.com/clients-requests", {
-            headers: {
-                'Access-Control-Allow-Origin': 'https://investment.netlify.app',
-                'Accept': '*',
-            }
-        })
-        .then((response) => {
-            if(response.data['requests']) {
-                for(const key in response.data['requests']) {
-                    loadedRequestsList.push({
-                        name: response.data['requests'][key].user_fullname,
-                        idNumber: response.data['requests'][key].user_id_number,
-                        phoneNumber: response.data['requests'][key].user_phone_number,
-                        refundAmount: response.data['requests'][key].refund_amount,
-                        bankAccount: response.data['requests'][key].user_bank,
-                        cardNumber: response.data['requests'][key].user_card_number,
-                    });
-                }
-                console.log(response.data['requests']);
-                setRequests(loadedRequestsList);
-            }
-        }).catch((error) => {
-            console.log(error);
-        });
-    };
-
     return (
         <div className="dashboard">
             <Sidebar />
             <section className="home-section">
-                <Route path="/dashboard/home">
-                    <Home requests={requests} />
-                </Route>
+                <Route path="/dashboard/home" component={Home} />
                 <Route path="/dashboard/change-service-name" component={CompanyNameForm} />
                 <Route path="/dashboard/requests" component={Requests} />
                 <Route path="/dashboard/add-new-bank" component={BankListFrom} />
